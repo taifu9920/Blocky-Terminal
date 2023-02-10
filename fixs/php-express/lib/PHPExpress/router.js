ae5dce221bd0fca170cc53b83c55ee1cfce2b01b
黎サブ 星野 <taifu9920923@gmail.com>
2023-02-10 23:54:19 +0800

    Update

---
+++ fixs/php-express/lib/PHPExpress/router.js
@@ -0,0 +1,8 @@
+module.exports = function(req, res) {
+    res.render(req.path.slice(1), {
+        method: req.method,
+        get: req.query,
+        post: req.body,
+		files: req.files
+    });
+};
