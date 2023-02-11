f7ec82c238ac903b4d68694d7525881f6240c097
黎サブ 星野 <taifu9920923@gmail.com>
2023-02-11 11:38:55 +0800

    Update

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,7 @@
 const { spawn } = require("cross-spawn");
 
 if (process.platform === "linux") {
-  spawn("cp", ["-R", "./fixs/*", "./node_modules/"]);
+  spawn("cp", ["-R", "../fixs/*", "../node_modules/"]);
 } else {
   spawn("xcopy", [".\\fixs\\", ".\\node_modules\\", "/E", "/I", "/Y"]);
 }
