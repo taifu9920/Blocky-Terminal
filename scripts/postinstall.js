f3f4df6d878844bf6dd75ea6f05191d0d0e2a9cd
黎サブ 星野 <taifu9920923@gmail.com>
2023-02-11 11:49:21 +0800

    Update

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,26 @@
-const { spawn } = require("cross-spawn");
+const fs = require('fs');
+const path = require('path');
 
-if (process.platform === "linux") {
-  spawn("cp", ["-R", "../fixs/*", "../node_modules/"]);
-} else {
-  spawn("xcopy", [".\\fixs\\", ".\\node_modules\\", "/E", "/I", "/Y"]);
+const source = 'fixs';
+const destination = 'node_modules';
+
+function copyFolderSync(source, destination) {
+  const files = fs.readdirSync(source);
+
+  if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination);
+  }
+
+  for (const file of files) {
+    const curSource = path.join(source, file);
+    const curDestination = path.join(destination, file);
+
+    if (fs.lstatSync(curSource).isDirectory()) {
+      copyFolderSync(curSource, curDestination);
+    } else {
+      fs.copyFileSync(curSource, curDestination);
+    }
+  }
 }
+
+copyFolderSync(source, destination);
